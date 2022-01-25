4ee464cc3bf5b14741f90fef2922ec69d343e78a
HeoJongWon <37110949+HeoJongWon@users.noreply.github.com>
2022-01-26 04:01:41 +0900

    solidity

---
+++ solidity/contracts/Ipfs.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Ipfs{
+    mapping(string=>string) ipfsAddress;
+
+    function getIpfsAddress(string memory date) public view returns(string memory) {
+        return ipfsAddress[date];
+    }
+
+    function setIpfsAddress(string memory date, string memory ipfs) public {
+        ipfsAddress[date] = ipfs;
+    }
+}
---
+++ solidity/truffle-config.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
+const NETWORK_ID = "1001";
+const GASLIMIT = "20000000";
+const URL = `https://api.baobab.klaytn.net:8651`;
+const PRIVATE_KEY =
+  "0x8cafa33df8c1740720bc4815ce7c7cd61d18aaf396bb2a3da5e197f0c7b85aff";
+
+module.exports = {
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+    },
+
+    klaytn: {
+      provider: new HDWalletProvider(PRIVATE_KEY, URL),
+      network_id: NETWORK_ID,
+      gas: GASLIMIT,
+      gasPrice: null,
+    },
+  },
+};
